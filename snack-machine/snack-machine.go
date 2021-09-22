7d3ec959764070c00c41a6eb34d8a9a35da50fd9
Alessandro Sangalli <alessandrosangalli@vipcommerce.com.br>
2021-09-22 00:22:40 -0300

    feat: snack machine

---
+++ snack-machine/snack-machine.go
@@ -0,0 +1,26 @@
+package snack_machine
+
+import (
+	"ddd-snack-machine/money"
+
+	"github.com/shopspring/decimal"
+)
+
+type SnackMachine struct {
+	insertedMoney []money.Money
+}
+
+func NewSnackMachine(inputMoney ...money.Money) *SnackMachine {
+	snackMachine := SnackMachine{insertedMoney: inputMoney}
+	return &snackMachine
+}
+
+func (snackMachine *SnackMachine) GetTotalMoney() decimal.Decimal {
+	total := decimal.NewFromInt(0)
+
+	for _, m := range snackMachine.insertedMoney {
+		total = decimal.Sum(total, m.Value)
+	}
+
+	return total
+}
