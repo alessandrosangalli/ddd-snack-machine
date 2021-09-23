70d3e0d62113f147c8443a5a61f782676b6dc309
Alessandro Sangalli <alessandrosangalli@vipcommerce.com.br>
2021-09-22 23:14:51 -0300

    feat: add snack buy to snack machine

--- snack-machine/snack-machine.go
+++ snack-machine/snack-machine.go
@@ -2,12 +2,15 @@ package snack_machine
 
 import (
 	"ddd-snack-machine/money"
+	"ddd-snack-machine/snack"
+	errors "ddd-snack-machine/snack-machine/errors"
 
 	"github.com/shopspring/decimal"
 )
 
 type SnackMachine struct {
 	insertedMoney []money.Money
+	snacks        []snack.Snack
 }
 
 func NewSnackMachine(inputMoney ...money.Money) *SnackMachine {
@@ -23,4 +26,26 @@ func (snackMachine *SnackMachine) getTotalMoney() decimal.Decimal {
 	}
 
 	return total
+}
+
+func (snackMachine *SnackMachine) AddSnack(snack ...snack.Snack) {
+	snackMachine.snacks = append(snackMachine.snacks, snack...)
+}
+
+func (snackMachine *SnackMachine) Buy(snack snack.Snack) (bool, error) {
+	if !snackMachine.snackIsAvaliable(snack) {
+		return false, errors.NewInsufficientStock(snack)
+	}
+
+	return true, nil
+}
+
+func (snackMachine *SnackMachine) snackIsAvaliable(snack snack.Snack) bool {
+	for _, s := range snackMachine.snacks {
+		if s.Name == snack.Name {
+			return true
+		}
+	}
+
+	return false
 }
