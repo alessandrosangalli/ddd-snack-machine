021e42984a944eda009cdb96ac3b4c0aee5e43b1
Alessandro Sangalli <alessandrosangalli@vipcommerce.com.br>
2021-09-22 21:24:31 -0300

    feat: test if inserted money stay right in snack machine

--- snack-machine/snack-machine_test.go
+++ snack-machine/snack-machine_test.go
@@ -2,6 +2,7 @@ package snack_machine
 
 import (
 	"ddd-snack-machine/money"
+	"reflect"
 	"testing"
 
 	"github.com/shopspring/decimal"
@@ -21,5 +22,44 @@ func TestTotalMoney(t *testing.T) {
 		expectedValue, _ := expected.Float64()
 		totalMoneyValue, _ := totalMoney.Float64()
 		t.Errorf("Error on total value, expected: %f, current: %f", expectedValue, totalMoneyValue)
+	}
+}
+
+func TestInsertedMoneyAllTypes(t *testing.T) {
+	moneyToInsert := []money.Money{
+		money.OneCent(),
+		money.TenCents(),
+		money.TwentyFiveCents(),
+		money.OneDollar(),
+		money.FiveDollars(),
+		money.TwentyDollars(),
+	}
+
+	snackMachine := NewSnackMachine(moneyToInsert...)
+
+	if !reflect.DeepEqual(snackMachine.insertedMoney, moneyToInsert) {
+		t.Errorf("Inserted money is not equal snack machine money")
+	}
+}
+
+func TestInsertedMoneyFiveDollars(t *testing.T) {
+	moneyToInsert := []money.Money{
+		money.TwentyDollars(),
+	}
+
+	snackMachine := NewSnackMachine(moneyToInsert...)
+
+	if !reflect.DeepEqual(snackMachine.insertedMoney, moneyToInsert) {
+		t.Errorf("Inserted money is not equal snack machine money")
+	}
+}
+
+func TestInsertedMoneyEmpty(t *testing.T) {
+	moneyToInsert := []money.Money{}
+
+	snackMachine := NewSnackMachine(moneyToInsert...)
+
+	if !reflect.DeepEqual(snackMachine.insertedMoney, moneyToInsert) {
+		t.Errorf("Inserted money is not equal snack machine money")
 	}
 }
